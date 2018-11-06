f6f4b048843b9a9a617729c7e38e1d8bad35707c
Unknown <remiles@wpi.edu>
2018-11-05 20:19:02 -0500

    Added slider live update

--- reflex-ros-pkg/rqt_gui_control/src/rqt_gui_control/glove_cntrl_widget.py
+++ reflex-ros-pkg/rqt_gui_control/src/rqt_gui_control/glove_cntrl_widget.py
@@ -1,10 +1,11 @@
 import rospy
-from python_qt_binding.QtWidgets import * #QWidget, QToolTip,QPushButton,QLabel,QGridLayout,QLineEdit
-
+from python_qt_binding.QtWidgets import *
+from std_msgs.msg import Int16MultiArray
 
 class GloveWidget(QWidget):
     def __init__(self):
         super(GloveWidget, self).__init__()
+        rospy.Subscriber('/glove_data',Int16MultiArray, self.processData())
         self.initUI()
 
 
@@ -17,6 +18,21 @@ class GloveWidget(QWidget):
         self.calibrate.addWidget(self.calibrate_glove)
         self.fbox = QFormLayout()
         self.fbox.addRow(QLabel(""), self.record)
+        self.fbox.addRow(QLabel("\n\n\nFor best results, calibrate glove before use"))
         self.fbox.addRow(QLabel(""), self.calibrate)
-        self.fbox.setVerticalSpacing(50)
         self.setLayout(self.fbox)
+
+        self.record_button.clicked.connect(self.recordGloveStart)
+        self.calibrate_glove.clicked.connect(self.calibrateGlove)
+
+    def recordGloveStart(self):
+        print "record glove movements"
+        if(self.record_button.text() == "Start Recording"):
+            self.record_button.setText("Stop Recording")
+        else:
+            self.record_button.setText("Start Recording")
+    def calibrateGlove(self):
+        print "calibrate glove"
+
+    def processData(self):
+        print "received data"
